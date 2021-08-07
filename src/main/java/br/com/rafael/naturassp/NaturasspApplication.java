a2dd85ca77fb14ebd16fb514dc88b69a700726c0
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2021-08-06 23:38:47 -0300

    Colocando autenticação no Swagger

--- src/main/java/br/com/rafael/naturassp/NaturasspApplication.java
+++ src/main/java/br/com/rafael/naturassp/NaturasspApplication.java
@@ -2,21 +2,28 @@ package br.com.rafael.naturassp;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import io.swagger.models.auth.In;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -50,7 +57,9 @@ public class NaturasspApplication {
 		.apis(RequestHandlerSelectors.basePackage("br.com.rafael.naturassp.controller"))
 		.paths(PathSelectors.any())
 		.build()
-		.apiInfo(apiInfo());
+		.apiInfo(apiInfo())
+		 .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+	        .securityContexts(Arrays.asList(securityContext()));
 	}
 	
 	private ApiInfo apiInfo() {
@@ -69,6 +78,21 @@ public class NaturasspApplication {
 	private ResponseMessage simpleMessage(int code, String msg) {
 		return new ResponseMessageBuilder().code(code).message(msg).build();
 	}
+	
+	 private List<SecurityReference> defaultAuth() {
+		    AuthorizationScope authorizationScope = new AuthorizationScope(
+		        "global", "accessEverything");
+		    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		    authorizationScopes[0] = authorizationScope;
+		    return Arrays.asList(new SecurityReference("apiKey",
+		        authorizationScopes));
+		    }
+	 
+	 
+	   private SecurityContext securityContext() {
+	        return SecurityContext.builder().securityReferences(defaultAuth())
+	            .forPaths(PathSelectors.ant("/pedido/**")).build();
+	    }
 	
 	
 
